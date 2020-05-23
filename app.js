71b4b9986e1798ec0561a14cf45322e612248670
hieu-ng <hieutnguyen06@gmail.com>
2020-05-23 10:17:12 +1000

    Fetching data using async await

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+}
