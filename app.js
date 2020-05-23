0e73769aba406599f5c7f0eb643399d5ea2ed811
hieu-ng <hieutnguyen06@gmail.com>
2020-05-23 10:51:49 +1000

    Add function to all buttons

--- app.js
+++ app.js
@@ -39,6 +39,29 @@ function addData(obj) {
     updateDOM();
 }
 
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML =
+        `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 function updateDOM(providedData = data) {
     // Clear main div
     main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>'
